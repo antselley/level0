e9cab8bc70fef9d55472d15621b010bbe21836d4
antselley <antselley@gmail.com>
2022-05-05 16:34:01 +0200

    First 3 tasks done

--- level0.py
+++ level0.py
@@ -1,1 +1,36 @@
 #test 12
+
+##### Task 0.1
+# x = 0
+# y = 1
+# print(x)
+# print(y)
+# x = x + 3
+# y = y + x
+# print(x)
+# print(y)
+
+#Variable assignment just updates what the variable (e.g. x) points to. In this case we change it to point to the integer object 3, so when we add it to y, we are adding 3 rather than the originally assigned value of 0.
+
+
+##### Task 0.2
+# x = 1 + 1 * 2
+# y = (1 + 1) * 2
+# z = 1 + ( 1 * 2 )
+# a = 1 + 1 * 2 / 2
+# b = (1 + 1 * 2 ) /  2
+
+# print("x=",x)
+# print("y=",y)
+# print("z=",z)
+# print("a=",a)
+# print("b=",b)
+
+##### Task 0.3
+def hello(input_string): 
+    print("Hello ", input_string, "!")
+
+hello("Tshepo")
+#Not sure if you wanted it to return the output, or print it. Opted for the printing option. 
+
+##### Task 0.4
