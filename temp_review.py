dcb5c96676e1f1318f951f5ef1de4f15b47ba502
antselley <antselley@gmail.com>
2022-05-13 06:14:42 +0200

    removed excess files

--- temp_review.py
+++
@@ -1,7 +0,0 @@
-def area_triangle_calc(side_one, side_two, side_three):
-    half_perimeter = (side_one + side_two + side_three) / 2
-    area = (half_perimeter * (half_perimeter - side_one) * (half_perimeter - side_two) * (half_perimeter - side_three) ) ** 0.5
-    return area 
-
-
-print(area_triangle_calc(3,4,5))
